81dd449f5710477d7d61bc58ba978f87f932f14d
cwjki <cwjki97@gmail.com>
2023-05-27 18:49:21 -0400

    importing the getSortedPostData into the getStaticProps on the index page

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,19 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 
+import { getSortedPostsData } from "../lib/posts";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
